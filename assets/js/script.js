c7bfe543b58c36bebac4e21999e3367b1e4b64ee
ateeqahmed11 <ateeqahmed11@yahoo.com>
2023-12-31 02:38:59 +0600

    Blog, client feedback contact and footer fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 (function($){
 	'use script';
 
+    $('.scroll-area').click(function(){
+        $('html').animate({
+          'scrollTop' : 0,
+        },700);
+        return false;
+  });
+  $(window).on('scroll',function(){
+        var a = $(window).scrollTop();
+        if(a>400){
+          $('.scroll-area').slideDown(300);
+      }else{
+          $('.scroll-area').slideUp(200);
+      }
+  });
+
     var mixer = mixitup('.portfolio-full');
 	var mixer = mixitup('.portF', {
 		selectors: {
@@ -17,5 +32,24 @@
           enabled: true
         }
   });
+  $(".client-review-full").owlCarousel({
+    loop:true,
+    center:false,
+    items:2,
+    autoplay: true,
+    mouseDrag:false,
+    nav:true,
+    navText:['<span class="client-review-arrow"><i class="fas fa-chevron-left"></i></span>','<span class="client-review-arrow"><i class="fas fa-chevron-right"></i></span>'],
+    smartSpeed:700,
+    autoplayTimeout:7000,
+    responsive:{
+        0:{
+            items:1
+        },
+        867:{
+            items:2
+        }
+    }
+});
 
 }(jQuery))
